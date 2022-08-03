35c1c0cc0aba7e3620d82664fc51cd987629c7ca
Katreina <101986864+Katreina@users.noreply.github.com>
2022-08-03 11:57:31 +0300

    initial commit pit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  console.log("first");
+  return (
+    <div className="App">
+      <div className="header">Money calculator</div>
+      <div className="main">Project body</div>
+    </div>
+  );
+}
+
+export default App;
