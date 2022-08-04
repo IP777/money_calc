f31575d124790bc6e436a974fcd1f6bf455d8ffc
IP777 <ivanov_piter@ukr.net>
2022-08-04 14:58:42 +0300

    add person list

---
+++ src/DB/personListDb.js
@@ -0,0 +1,112 @@
+const initPersonArr = [
+  {
+    id: "4",
+    name: "Пацай Роман",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "1",
+    name: "Iванов Петро",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "2",
+    name: "Ступарь Олександр",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "3",
+    name: "Слободянюк Дмитрiй",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "5",
+    name: "Марусяк Роман",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "6",
+    name: "Чабан Антон",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "7",
+    name: "Голосов Денис",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "8",
+    name: "Синютка Микола",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "9",
+    name: "Бубiсь Василь",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "10",
+    name: "Ластовецький Богдан",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "11",
+    name: "Голод Юрiй",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "12",
+    name: "Гуменчук Роман",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "13",
+    name: "Кравченко Олег",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "14",
+    name: "Воронiн Валерiй",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "15",
+    name: "Вігнан Євген",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "16",
+    name: "Сисоєв Володимир",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "17",
+    name: "Строколіс Андрій",
+    money: "",
+    checked: false,
+  },
+  {
+    id: "18",
+    name: "Прядка Юрій",
+    money: "",
+    checked: false,
+  },
+];
+
+export default initPersonArr;
